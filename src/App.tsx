d9231a63fc0ab572b9a4abd1c336977e9685588c
vntsmatos <ovinidev@gmail.com>
2022-06-06 19:32:23 -0300

    @vntsmatos[Task:Projeto blog pessoal]{06/06/2022|19:32}

---
+++ src/App.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './componentes/estaticos/navbar/Navbar';
+import Footer from './componentes/estaticos/footer/Footer';
+import './App.css';
+import Home from './paginas/home/Home';
+import Login from './paginas/login/Login';
+import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
+import ListaTema from './componentes/temas/listaTema/ListaTema';
+import ListaPostagem from './componentes/postagens/listaPostagem/listaPostagem';
+import CadastroPostagem from './componentes/postagens/cadastroPostagem/CadastroPostagem';
+import CadastroTema from './componentes/temas/cadastroTema/CadastroTema';
+import DeletarPostagem from './componentes/postagens/deletarPostagem/DeletarPostagem';
+import DeletarTema from './componentes/temas/deletarTema/DeletarTema';
+import { Provider } from 'react-redux';
+import store from './store/store';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Login />} />
+
+          <Route path="/home" element={<Home />} />
+
+          <Route path="/login" element={<Login />} />
+
+          <Route path="/cadastrousuario" element={<CadastroUsuario />} />
+
+          <Route path="/temas" element={<ListaTema />} />
+
+          <Route path="/posts" element={<ListaPostagem />} />
+
+          <Route path="/formularioPostagem" element={<CadastroPostagem />} />
+
+          <Route path="/formularioPostagem/:id" element={<CadastroPostagem />} />
+
+          <Route path="/formularioTema" element={<CadastroTema />} />
+
+          <Route path="/formularioTema/:id" element={<CadastroTema />} />
+
+          <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
+
+          <Route path="/deletarTema/:id" element={<DeletarTema />} />
+
+        </Routes>
+        <Footer />
+      </Router>
+    </Provider>
+  );
+}
+
+export default App;
